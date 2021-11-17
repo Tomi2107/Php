1db005845a0f11421932908b460761110008f6a5
Tomi2107 <83030653+Tomi2107@users.noreply.github.com>
2021-11-17 20:50:00 -0300

    Add files via upload

---
+++ Php.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <?php
+    
+    $nombre ="Tomas";
+    $apellido = "Palmeyro";
+    $edad = "39";
+    $hobbies = "Programar";
+    $editordecodigo = "VSC";
+    $sistemaopertativo = "Win10";
+
+    echo $nombre;
+    echo $apellido;
+    echo $edad;
+    echo $hobbies;
+    echo $editordecodigo;
+    echo $sistemaopertativo;
+    
+    echo"<p>$nombre, $apellido, $edad, $hobbies, $editordecodigo, $sistemaopertativo</p> "
+    ?>
+</body>
+
+</html>
